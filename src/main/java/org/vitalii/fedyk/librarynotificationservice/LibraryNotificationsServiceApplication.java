7f0500a45344251a942ff69d76f65dc9f4803b2f
fedykvitalik2004 <90273574+fedykvitalik2004@users.noreply.github.com>
2025-01-03 11:46:02 +0200

    Temporary

--- src/main/java/org/vitalii/fedyk/librarynotificationservice/LibraryNotificationsServiceApplication.java
+++ src/main/java/org/vitalii/fedyk/librarynotificationservice/LibraryNotificationsServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryNotificationsServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LibraryNotificationsServiceApplication.class, args);
     }
-
 }
